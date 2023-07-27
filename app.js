f23e7b4e080ef2ed32cdf5ab6eff9a4fe0e5c7a8
Karan Adwani <developedbykaran18@gmail.com>
2023-07-27 12:40:05 +0530

    functionallity added

---
+++ app.js
@@ -0,0 +1,33 @@
+const outputValue = document.querySelector('#output');
+let expression = "";
+let calculated = false;
+
+let clr = ()=>{
+    outputValue.value = "";
+}
+
+let display = (num)=>{
+    if(calculated)
+    {
+        outputValue.value = ""
+        calculated = false;
+        outputValue.value += num;
+    }
+    else if(!calculated)
+    {
+        outputValue.value += num;
+    }
+}
+
+let calculate = ()=>{
+    try{
+        expression = eval(outputValue.value); //eval function is used to evaluate the mathematical
+        calculated = true;
+    }
+    catch{
+        alert("Invalid Input");
+    }
+    finally{
+        outputValue.value = expression;
+    }
+}
